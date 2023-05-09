18db0f83ab7a161f6663b8d8c02befcf472056c4
CatzillaW <harodonw@student.21-school.ru>
2023-05-09 09:06:15 +0300

    easy gpt bot success

--- gpt_aiogram_light/maingpt.py
+++ gpt_aiogram_light/maingpt.py
@@ -8,7 +8,11 @@ from aiogram.dispatcher.filters import Text
 from aiogram.types import ContentType, File
 from bot_keyboard import start_keyboard
 from pathlib import Path
+import speech_recognition as rc
+import pyglet
 
+
+recognizer = rc.Recognizer()
 ai.api_key = OPEN_AI_KEY
 messages  = [
     {"role": "system", "content": "You are a programming assistant at Proghunter.ru, helping users with Python and JavaScript programming with popular frameworks."},
@@ -37,19 +41,35 @@ async def start_mess(message: types.Message) -> None:
                          reply_markup=start_keyboard, parse_mode='HTML')
     await message.delete()
 
+
+# РАСПОЗНАВАНИЕ АУДИО
+def audio_to_text(filename):
+    with rc.AudioFile(filename) as source:
+        audio = recognizer.record(source)
+    try:
+        return recognizer.recognize_google(audio)
+    except:
+        print("Skipping unknown error")
+
+
 # ПРИЁМ АУДИО
 async def handle_file(file: File, file_name: str, path: str):
     Path(f"{path}").mkdir(parents=True, exist_ok=True)
+    audio_file = await init_bot.download_file(file_path=file.file_path, destination=f"{path}/{file_name}")
+    return audio_file
 
-    await init_bot.download_file(file_path=file.file_path, destination=f"{path}/{file_name}")
-
-
+# не понятно пока что как открывать файл в папке - нужно распознать текст в audio_to_text
+# !!!!!!!!!!!!!!!!!!!
 @disp_bot.message_handler(content_types=[ContentType.VOICE])
 async def any_message_from_user(message: types.Message) -> None:
     voice = await message.voice.get_file()
     path = "files/voices/"
-
-    await handle_file(file=voice, file_name=f"{voice.file_id}.ogg", path=path)
+    audio_file = await handle_file(file=voice, file_name=f"{voice.file_id}.ogg", path=path)
+    
+    song = pyglet.media.load(audio_file)
+    audio_text = audio_to_text(audio_file)
+    content = any_message_from_user(audio_text)
+    any_message_from_user(content)
 
 
     # задумка, чтобы писать какую-либо фразу перед ответом о том, что как будето АИ размышляет
@@ -58,20 +78,13 @@ async def any_message_from_user(message: types.Message) -> None:
 @disp_bot.message_handler()
 async def any_message_from_user(message: types.Message) -> None:
     update(messages, "user", message.text)
-    
+    print("generate_response_text")    
     response = ai.ChatCompletion.create(
         model="gpt-3.5-turbo",  # Новая модель с контекстом
         messages=messages,   # База данных на основе словаря
     )
     resolutipn_response = response['choices'][0]['message']['content'] + "\n" + str(len(response))
-
     await message.answer(resolutipn_response, parse_mode="markdown")
-    
-    print("generate_response_text")
-    completions = ai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=message.text
-    )
 
 
 if __name__ == "__main__":
